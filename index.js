213d512b4ebffa5fdfee7f5e4763717c95698ae5
Kai Ransome <ransomekai@gmail.com>
2018-03-28 20:26:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var katzDeli = [];
+
+function takeANumber (katzDeli, name) {
+  katzDeli.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
+
+}
+
+
+function nowServing (katzDeli) {
+  if (katzDeli.length > 0) {
+  return `Currently serving ${katzDeli.shift()}.`}
+  else {return "There is nobody waiting to be served!"}
+}
+
+function currentLine (katzDeli) {
+  
+  if (katzDeli.length > 0) {
+    return `The line is currently: ${katzDeli[0]}`
+  } else {return "The line is currently empty."}
+}
